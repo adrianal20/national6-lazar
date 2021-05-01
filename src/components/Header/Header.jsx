694808c2453abd8d48a5d820acf2cc36c401e380
adriana <adrianaramona.lazar@gmail.com>
2021-05-02 01:38:27 +0300

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import { AppLogo } from "../AppLogo/AppLogo";
+import "./Header.css";
+
+export function Header() {
+    return ( <div className="app-header">
+        <AppLogo/> 
+    </div>
+    );
+}4
