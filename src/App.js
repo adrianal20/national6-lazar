596d7ca76c96df06b04359f9d42462b7783d90cc
adriana <adrianaramona.lazar@gmail.com>
2021-05-02 00:48:20 +0300

    second

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
